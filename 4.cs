55aa0f890e430247d8f493fad2cc00683bb53f6a
Ekaterina Khmaruk <ekaterinabond43@gmail.com>
2022-11-15 10:06:01 +0500

    Created repos

---
+++ 4.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает
+// все чётные числа от 1 до N.
+
+int a = 0;
+int i = 2;
+bool correct = false;
+
+while(!correct)
+{
+    Console.Write("Введите число: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    if (a < 2 )
+    {
+        Console.Write("Некорректное число, введите число больше 1. ");
+    }
+    else
+    {
+        correct = true;
+    }
+}
+
+Console.Write("Все четные числа от 1 до " + a + ": ");
+while (i <= a)
+
+{
+    Console.Write(" " + i);
+    i = i + 2 ;
+}
+
+    
+    
+
